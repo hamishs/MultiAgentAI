c4b7df0d78434c66214277057f59c9f34e18f239
hamishs <hamish.scott@icloud.com>
2021-04-21 15:56:12 +0100

    fix args overwrite

--- combat/utils.py
+++ combat/utils.py
@@ -7,6 +7,7 @@ class Loader:
             input_dict = input_obj
         else:
             input_dict = input_obj.__dict__
+        input_dict = {k:v for k,v in input_dict.items() if v is not None}
         with open('config.yml', 'r') as f:
             custom_params = yaml.safe_load(f)[input_dict['config_name']]
         combined_dict = {**custom_params, **input_dict}
@@ -20,6 +21,7 @@ class train_parser:
 
 	parser = argparse.ArgumentParser(description='MNMT Input Params, Training')
 
+	###### required arguments ######
 	parser.add_argument(
 	    '--run_name', required=True,
 	    type=str, help="Name of run"
@@ -28,90 +30,8 @@ class train_parser:
 	    '--config_name', default='base',
 	    type=str, help="Name of config params."
 	)
-	parser.add_argument(
-	    '--grid_shape', default=15,
-	    type=int, help="Size of grid."
-	)
-	parser.add_argument(
-	    '--n_agents', default=5,
-	    type=int, help="Number of agents (same for opponents)."
-	)
-	parser.add_argument(
-	    '--gamma', default=0.99,
-	    type=float, help="Discount rate."
-	)
-	parser.add_argument(
-	    '--lamda', default=0.7,
-	    type=float, help="Lambda return constant."
-	)
-	parser.add_argument(
-	    '--epsilon', default=0.2,
-	    type=float, help="Max clip in advantage."
-	)
-	parser.add_argument(
-	    '--v_weight', default=1.0,
-	    type=float, help="Weight of critic loss to actor loss."
-	)
-	parser.add_argument(
-	    '--e_weight', default=0.01,
-	    type=float, help="Strength of entropy regularisation."
-	)
-	parser.add_argument(
-	    '--buffer_size', default=300,
-	    type=int, help="Max episodes to store in buffer."
-	)
-	parser.add_argument(
-	    '--channels', default=32,
-	    type=int, help="Number of channels of CNN."
-	)
-	parser.add_argument(
-	    '--kernel_size', default=3,
-	    type=int, help="Kernel size of CNN."
-	)
-	parser.add_argument(
-	    '--hidden_size', default=32,
-	    type=int, help="Hidden size of MLP."
-	)
-	parser.add_argument(
-	    '--lr_actor', default=5e-3,
-	    type=float, help="Learning rate for actor network."
-	)
-	parser.add_argument(
-	    '--lr_critic', default=5e-3,
-	    type=float, help="Learning rate for critic network."
-	)
-	parser.add_argument(
-	    '--episodes', default=5000,
-	    type=int, help="Number of episodes to train for."
-	)
-	parser.add_argument(
-	    '--train_freq', default=1,
-	    type=int, help="Frequency of episodes with training."
-	)
-	parser.add_argument(
-	    '--train_steps', default=3,
-	    type=int, help="Number of gradient steps when training."
-	)
-	parser.add_argument(
-	    '--verbose', default=20,
-	    type=int, help="Frequency of episodes to print stats."
-	)
-	parser.add_argument(
-		'--activation', default='relu',
-		type=str, help="Activation function either relu or tanh."
-	)
-	parser.add_argument(
-		'--num_cnn_layers', default=3,
-		type=str, help="Activation function either relu or tanh."
-	)
-	parser.add_argument(
-		'--actor_layers', default=3,
-		type=str, help="Activation function either relu or tanh."
-	)
-	parser.add_argument(
-		'--critic_layers', default=2,
-		type=str, help="Activation function either relu or tanh."
-	)
+
+	###### action arguments ######
 	parser.add_argument(
 		'--wandb', action='store_true',
 		help="Will record run in weights and biases."
@@ -120,6 +40,93 @@ class train_parser:
 		'--save', action='store_true',
 		help="Save results and model."
 	)
+
+	###### optional arguments (overwrite config) ######
+	parser.add_argument(
+	    '--grid_shape', default=None,
+	    type=int, help="Size of grid."
+	)
+	parser.add_argument(
+	    '--n_agents', default=None,
+	    type=int, help="Number of agents (same for opponents)."
+	)
+	parser.add_argument(
+	    '--gamma', default=None,
+	    type=float, help="Discount rate."
+	)
+	parser.add_argument(
+	    '--lamda', default=None,
+	    type=float, help="Lambda return constant."
+	)
+	parser.add_argument(
+	    '--epsilon', default=None,
+	    type=float, help="Max clip in advantage."
+	)
+	parser.add_argument(
+	    '--v_weight', default=None,
+	    type=float, help="Weight of critic loss to actor loss."
+	)
+	parser.add_argument(
+	    '--e_weight', default=0.01,
+	    type=float, help="Strength of entropy regularisation."
+	)
+	parser.add_argument(
+	    '--buffer_size', default=None,
+	    type=int, help="Max episodes to store in buffer."
+	)
+	parser.add_argument(
+	    '--channels', default=None,
+	    type=int, help="Number of channels of CNN."
+	)
+	parser.add_argument(
+	    '--kernel_size', default=None,
+	    type=int, help="Kernel size of CNN."
+	)
+	parser.add_argument(
+	    '--hidden_size', default=None,
+	    type=int, help="Hidden size of MLP."
+	)
+	parser.add_argument(
+	    '--lr_actor', default=None,
+	    type=float, help="Learning rate for actor network."
+	)
+	parser.add_argument(
+	    '--lr_critic', default=None,
+	    type=float, help="Learning rate for critic network."
+	)
+	parser.add_argument(
+	    '--episodes', default=None,
+	    type=int, help="Number of episodes to train for."
+	)
+	parser.add_argument(
+	    '--train_freq', default=None,
+	    type=int, help="Frequency of episodes with training."
+	)
+	parser.add_argument(
+	    '--train_steps', default=None,
+	    type=int, help="Number of gradient steps when training."
+	)
+	parser.add_argument(
+	    '--verbose', default=None,
+	    type=int, help="Frequency of episodes to print stats."
+	)
+	parser.add_argument(
+		'--activation', default=None,
+		type=str, help="Activation function either relu or tanh."
+	)
+	parser.add_argument(
+		'--num_cnn_layers', default=None,
+		type=str, help="Activation function either relu or tanh."
+	)
+	parser.add_argument(
+		'--actor_layers', default=None,
+		type=str, help="Activation function either relu or tanh."
+	)
+	parser.add_argument(
+		'--critic_layers', default=None,
+		type=str, help="Activation function either relu or tanh."
+	)
+	
 
 	@staticmethod
 	def parse_args():
